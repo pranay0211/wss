1181fb3eccb1eee8b56b17046468bebb679729a7
pranay0211 <pranay123456789@gmail.com>
2023-10-12 13:29:31 +0400

    Update wss-vertx.java

--- wss-vertx.java
+++ wss-vertx.java
@@ -2,41 +2,23 @@ import io.vertx.circuitbreaker.CircuitBreaker;
 import io.vertx.circuitbreaker.CircuitBreakerOptions;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerOptions;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.sockjs.SockJSBridgeOptions;
 import io.vertx.ext.web.handler.sockjs.BridgeEventType;
 import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 import io.vertx.ext.web.handler.sockjs.PermittedOptions;
-import io.vertx.ext.web.handler.BodyHandler;
-import io.vertx.ext.web.handler.CorsHandler;
 import io.vertx.ext.ratelimiter.RateLimiter;
 import io.vertx.ext.ratelimiter.RateLimiterOptions;
+import io.vertx.core.json.JsonObject;
 
 public class WebSocketServerVerticle extends AbstractVerticle {
 
     @Override
     public void start() {
         Router router = Router.router(vertx);
-
-        // Enable CORS support
-        router.route().handler(CorsHandler.create("*"));
-
-        // Enable BodyHandler to handle request bodies
-        router.route().handler(BodyHandler.create());
-
-        // Set up rate limiting for WebSocket connections
-        RateLimiter rateLimiter = RateLimiter.create(vertx, new RateLimiterOptions()
-                .setLimit(10) // Set the maximum number of requests allowed per second
-                .setResetTimeout(1000) // Reset rate limit counters every second
-        );
-
-        // Set up the circuit breaker
-        CircuitBreakerOptions circuitBreakerOptions = new CircuitBreakerOptions()
-                .setMaxFailures(5) // Number of failures before opening the circuit
-                .setTimeout(2000) // Time in milliseconds after which the operation is considered a failure
-                .setResetTimeout(5000); // Time in milliseconds circuit breaker should wait before trying to re-engage (close the circuit) after a failure
-
-        CircuitBreaker circuitBreaker = CircuitBreaker.create("websocket-circuit-breaker", vertx, circuitBreakerOptions);
 
         // Enable WebSocket support
         SockJSBridgeOptions bridgeOptions = new SockJSBridgeOptions()
@@ -45,51 +27,74 @@ public class WebSocketServerVerticle extends AbstractVerticle {
 
         SockJSHandler sockJSHandler = SockJSHandler.create(vertx)
                 .bridge(bridgeOptions, event -> {
-                    // Handle onOpen event
                     if (event.type() == BridgeEventType.SOCKET_CREATED) {
-                        rateLimiter.acquire(1, res -> {
-                            if (res.succeeded()) {
-                                circuitBreaker.execute(future -> {
-                                    // Handle WebSocket connection logic here
-                                    // For example, sending a welcome message to the client
-                                    event.socket().write("Welcome to the WebSocket server!");
-
-                                    // Set up the onClose event handler
-                                    event.socket().closeHandler(close -> {
-                                        // Handle the WebSocket connection closing here
-                                        System.out.println("WebSocket connection closed.");
-                                    });
-
-                                    future.complete(true);
-                                }, throwable -> {
-                                    // Circuit breaker is open, return a fallback response
-                                    event.complete(false);
-                                    return null;
-                                });
-                            } else {
-                                // Rate limit exceeded, reject the connection
-                                event.complete(false);
-                            }
-                        });
+                        // Handle onConnect event
+                        vertx.eventBus().publish("websocket-log", new JsonObject()
+                                .put("type", "connect")
+                                .put("timestamp", System.currentTimeMillis())
+                                .put("sessionId", event.socket().webSession().id()));
+                    } else if (event.type() == BridgeEventType.SOCKET_CLOSED) {
+                        // Handle onClose event
+                        vertx.eventBus().publish("websocket-log", new JsonObject()
+                                .put("type", "close")
+                                .put("timestamp", System.currentTimeMillis())
+                                .put("sessionId", event.socket().webSession().id()));
+                    } else if (event.type() == BridgeEventType.UNREGISTER) {
+                        // Handle onError event (socket was closed due to an error)
+                        vertx.eventBus().publish("websocket-log", new JsonObject()
+                                .put("type", "error")
+                                .put("timestamp", System.currentTimeMillis())
+                                .put("sessionId", event.socket().webSession().id())
+                                .put("cause", event.cause() != null ? event.cause().getMessage() : "Unknown Error"));
                     }
+
+                    // Handle other WebSocket events if needed
                 });
 
         router.route("/websocket/*").handler(sockJSHandler);
 
         // Start the HTTP server
-        vertx.createHttpServer()
-            .requestHandler(router)
-            .listen(8080, ar -> {
-                if (ar.succeeded()) {
-                    System.out.println("WebSocket server started on port 8080");
-                } else {
-                    System.out.println("Failed to start WebSocket server: " + ar.cause().getMessage());
-                }
-            });
+        HttpServerOptions serverOptions = new HttpServerOptions().setPort(8080);
+        HttpServer httpServer = vertx.createHttpServer(serverOptions);
+        httpServer.requestHandler(router).listen();
+
+        // Set up rate limiting for WebSocket connections
+        RateLimiter rateLimiter = RateLimiter.create(vertx, new RateLimiterOptions()
+                .setLimit(10) // Set the maximum number of requests allowed per second
+                .setResetTimeout(1000) // Reset rate limit counters every second
+        );
+
+        // Set up the circuit breaker (if needed)
+        CircuitBreakerOptions circuitBreakerOptions = new CircuitBreakerOptions()
+                .setMaxFailures(5) // Number of failures before opening the circuit
+                .setTimeout(2000) // Time in milliseconds after which the operation is considered a failure
+                .setResetTimeout(5000); // Time in milliseconds circuit breaker should wait before trying to re-engage (close the circuit) after a failure
+
+        CircuitBreaker circuitBreaker = CircuitBreaker.create("websocket-circuit-breaker", vertx, circuitBreakerOptions);
+
+        // Register event bus consumer for communication between Vert.x instances
+        EventBus eventBus = vertx.eventBus();
+        eventBus.consumer("websocket-message-address", message -> {
+            // Handle messages sent from other Vert.x instances
+        });
+
+        // Event bus consumer for handling WebSocket logs
+        eventBus.consumer("websocket-log", message -> {
+            // Handle WebSocket log messages (connect, close, error)
+            JsonObject logMessage = (JsonObject) message.body();
+            System.out.println("WebSocket Event: " + logMessage.encode());
+        });
     }
 
     public static void main(String[] args) {
         Vertx vertx = Vertx.vertx();
+
+        // Deploy multiple instances of the WebSocketServerVerticle across different nodes
         vertx.deployVerticle(new WebSocketServerVerticle());
+
+        // Example of sending a message from one Vert.x instance to another (for distributed communication)
+        vertx.setPeriodic(5000, id -> {
+            vertx.eventBus().publish("websocket-message-address", "Hello from distributed Vert.x instance!");
+        });
     }
 }
