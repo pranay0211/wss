bc6feb4cffe48c5d877cd984eaf0a4b2e132bb12
pranay0211 <pranay123456789@gmail.com>
2023-10-12 13:16:11 +0400

    Update wss-vertx.java

--- wss-vertx.java
+++ wss-vertx.java
@@ -1,50 +1,62 @@
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
-import io.vertx.core.http.HttpClient;
-import io.vertx.core.http.HttpClientOptions;
-import io.vertx.core.http.WebSocket;
-import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.http.ServerWebSocket;
+import io.vertx.core.json.JsonObject;
 
-public class WebSocketClientVerticle extends AbstractVerticle {
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class WebSocketServerVerticle extends AbstractVerticle {
+
+    private Map<String, ServerWebSocket> sessions = new ConcurrentHashMap<>();
 
     @Override
     public void start() {
-        HttpClientOptions options = new HttpClientOptions()
-                .setSsl(true)
-                .setTrustAll(true);
+        HttpServerOptions serverOptions = new HttpServerOptions().setSsl(true); // SSL enabled, configure with your certificate
+        HttpServer httpServer = vertx.createHttpServer(serverOptions);
 
-        HttpClient client = vertx.createHttpClient(options);
+        httpServer.webSocketHandler(webSocket -> {
+            String sessionId = generateSessionId();
+            sessions.put(sessionId, webSocket);
 
-        client.webSocket(443, "example.com", "/socket", webSocket -> {
-            if (webSocket.succeeded()) {
-                WebSocket socket = webSocket.result();
+            // Handle messages from clients
+            webSocket.handler(buffer -> {
+                System.out.println("Received message from client: " + buffer.toString());
+                // Process the message and send a response if needed
+            });
 
-                socket.handler(data -> {
-                    System.out.println("Received message: " + data.toString("UTF-8"));
-                });
+            // Handle WebSocket closure
+            webSocket.closeHandler(close -> {
+                System.out.println("WebSocket closed for session: " + sessionId);
+                sessions.remove(sessionId);
+            });
 
-                socket.exceptionHandler(e -> {
-                    System.out.println("WebSocket error: " + e.getMessage());
-                    // Handle connection errors here
-                });
+            // Handle exceptions
+            webSocket.exceptionHandler(e -> {
+                System.out.println("WebSocket error for session " + sessionId + ": " + e.getMessage());
+                sessions.remove(sessionId);
+            });
+        });
 
-                socket.closeHandler(close -> {
-                    System.out.println("WebSocket closed with status code: " + close.statusCode() + ", reason: " + close.reason());
-                    // Handle connection closure here
-                });
-
-                // Periodically send ping messages to keep the session alive
-                vertx.setPeriodic(5000, timerId -> {
-                    socket.writePing(Buffer.buffer("Ping"));
-                });
+        httpServer.listen(443, result -> {
+            if (result.succeeded()) {
+                System.out.println("WebSocket server started");
             } else {
-                System.out.println("Failed to connect: " + webSocket.cause().getMessage());
+                System.out.println("WebSocket server failed to start: " + result.cause().getMessage());
             }
         });
     }
 
+    private String generateSessionId() {
+        // Implement your session ID generation logic here
+        // For example, you can use UUID.randomUUID().toString()
+        return "some-unique-session-id";
+    }
+
     public static void main(String[] args) {
         Vertx vertx = Vertx.vertx();
-        vertx.deployVerticle(new WebSocketClientVerticle());
+        vertx.deployVerticle(new WebSocketServerVerticle());
     }
 }
