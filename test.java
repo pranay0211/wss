8d48c558a86a94e2d61ffeab59b29eedd6bd51e9
pranay0211 <pranay123456789@gmail.com>
2023-10-12 13:39:05 +0400

    Update test.java

--- test.java
+++ test.java
@@ -27,3 +27,76 @@
             }
         });
     }
+@Test
+    void testWebSocketConnection(Vertx vertx, VertxTestContext testContext) {
+        // Deploy the WebSocket server verticle
+        vertx.deployVerticle(new WebSocketServerVerticle(), deployResult -> {
+            if (deployResult.succeeded()) {
+                // Create a WebSocket client and establish a connection
+                WebClient webClient = WebClient.create(vertx);
+                webClient.get(8080, "localhost", "/websocket/some/path")
+                        .as(BodyCodec.string())
+                        .send(ar -> {
+                            if (ar.succeeded()) {
+                                // Validate the response status code and content
+                                assertEquals(200, ar.result().statusCode());
+                                assertEquals("Welcome to the WebSocket server!", ar.result().body());
+                                testContext.completeNow();
+                            } else {
+                                // Connection failed, fail the test context
+                                testContext.failNow(ar.cause());
+                            }
+                        });
+            } else {
+                // Deployment failed, fail the test context
+                testContext.failNow(deployResult.cause());
+            }
+        });
+    }
+
+
+@Test
+    void testWebSocketEventHandling(Vertx vertx, VertxTestContext testContext) {
+        // Deploy the WebSocket server verticle
+        vertx.deployVerticle(new WebSocketServerVerticle(), deployResult -> {
+            if (deployResult.succeeded()) {
+                // Simulate WebSocket events (connect, close, error)
+                // Validate the event handling logic in the WebSocketServerVerticle
+
+                // Wait for the WebSocket events to be handled
+                vertx.setTimer(2000, timerId -> {
+                    // Validate the event handling logic
+                    // ...
+
+                    // Complete the test context
+                    testContext.completeNow();
+                });
+            } else {
+                // Deployment failed, fail the test context
+                testContext.failNow(deployResult.cause());
+            }
+        });
+    }
+
+    @Test
+    void testWebSocketMessageExchange(Vertx vertx, VertxTestContext testContext) {
+        // Deploy the WebSocket server verticle
+        vertx.deployVerticle(new WebSocketServerVerticle(), deployResult -> {
+            if (deployResult.succeeded()) {
+                // Establish WebSocket connections and exchange messages
+                // Validate message exchange logic in the WebSocketServerVerticle
+
+                // Wait for the message exchange to complete
+                vertx.setTimer(2000, timerId -> {
+                    // Validate the message exchange logic
+                    // ...
+
+                    // Complete the test context
+                    testContext.completeNow();
+                });
+            } else {
+                // Deployment failed, fail the test context
+                testContext.failNow(deployResult.cause());
+            }
+        });
+    }
